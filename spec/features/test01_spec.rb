8775afb1514589bb647437d25f32a93c02f2b850
Hubert Wong <hubertwwong@gmail.com>
2013-08-09 17:34:21 -0700

    some basic zip code searching is now working.

--- spec/features/test01_spec.rb
+++ spec/features/test01_spec.rb
@@ -1,8 +1,35 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe "googe", :type => :feature do
-  it "should sign me in" do
-    visit "http://www.google.com"
-    save_and_open_page
+describe "usps zip", :type => :feature do
+  it "be present" do
+    visit "https://tools.usps.com/go/ZipLookupAction!input.action"
+    
+    expect(page).to have_content 'Street Address'
+    #save_and_open_page
+  end
+  
+  it "can click on the zip code" do
+    visit "https://tools.usps.com/go/ZipLookupAction!input.action"
+    click_link("Cities by ZIP Code", exact: false)
+    
+    expect(page).to have_content 'Get a list of all the cities in a ZIP Code'
+  end
+  
+  it "success zip." do
+    visit "https://tools.usps.com/go/ZipLookupAction!input.action"
+    click_link("Cities by ZIP Code", exact: false)
+    fill_in 'tZip', :with => '91770'
+    click_button 'Find'
+    
+    expect(page).to have_content 'Rosemead'
+  end
+  
+  it "unsuccessful zip." do
+    visit "https://tools.usps.com/go/ZipLookupAction!input.action"
+    click_link("Cities by ZIP Code", exact: false)
+    fill_in 'tZip', :with => '00000'
+    click_button 'Find'
+    
+    expect(page).to have_content 'Sorry'
   end
 end 
