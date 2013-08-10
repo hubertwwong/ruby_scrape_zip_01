8497b3e3fe4307703eb980174fcc1943a0ed7743
Hubert Wong <hubertwwong@gmail.com>
2013-08-10 15:44:06 -0700

    added more stuff to the utility function. working on getting the insert working.

--- spec/features/sql_util_spec.rb
+++ spec/features/sql_util_spec.rb
@@ -5,11 +5,75 @@ require 'rubygems'
 
 describe SqlUtil do
   
-  describe "server_version" do
-    it "will return a string" do
-      s = SqlUtil.new
-      s.server_version
-    end 
+  describe "test_01 db" do
+    before(:each) do
+      user = 'root'
+      password = 'password'
+      url = 'localhost'
+      db_name = 'test01'
+      @db = SqlUtil.new(:url => url, :user=> user, :password => password, :db_name => db_name)
+    end
+    
+    describe "server_version" do
+      it "returns a string" do
+        result = @db.server_version
+        result.should include 'Server version'
+      end
+    end
+    
+    describe "num_rows" do
+      it "car db returns 3" do
+        result = @db.num_rows('car')
+        result.should == 3
+      end
+    end
+    
+    describe "read_last_row" do
+      it "car db order by make will return toyota" do
+        result = @db.read_last_row('car', 'make')
+        result['make'].should == 'toyota'
+      end
+    end
+    
+    describe "read_one_param" do
+      it "car db order by make will return toyota" do
+        result = @db.read_with_one_param('car', 'make', 'foo')
+        result['model'].should == 'bar'
+      end
+    end
+    
+    describe "col_names" do
+      it "car db returns id, name, model array" do
+        result = @db.read_col_names('car')
+        result.should == ['id', 'make', 'model']
+      end
+    end
+    
+    describe "hash_array" do
+      it "return a hash off 2 arrays" do
+        arr1 = ['a', 'b', 'c']
+        arr2 = ['d', 'e', 'f']
+        result = @db.hash_arrays(arr1, arr2)
+        result.should == {'a' => 'd', 'b' => 'e', 'c' => 'f'}
+      end
+    end
+    
+    describe "hash_get_keys_as_str" do
+      it "returns a comma seperated string" do
+        arr1 = {'a' => 'd', 'b' => 'e', 'c' => 'f'}
+        result = @db.hash_get_keys_as_str(arr1)
+        result.should == 'a, b, c'
+      end
+    end
+    
+    describe "hash_get_values_as_str" do
+      it "returns a space seperated string" do
+        arr1 = {'a' => 'd', 'b' => 'e', 'c' => 'f'}
+        result = @db.hash_get_keys_as_str(arr1)
+        result.should == 'a, b, c'
+      end
+    end
+    
   end
   
 end
--- src/sql_util.rb
+++ src/sql_util.rb
@@ -2,16 +2,200 @@ require 'mysql'
 
 class SqlUtil
   
+  attr_accessor :url, :user, :password, :db_name
+  
+  # need to pass all of the db args for the methods to work.
+  def initialize(params = {})
+    @url = params.fetch(:url)
+    @user = params.fetch(:user)
+    @password = params.fetch(:password)
+    @db_name= params.fetch(:db_name)
+  end
+  
+  # INSERT METHODS
+  ############################################################################
+  
+  # BROKEN....
+  # inserts a single row into db. 
+  # takes a hash...
+  # assumes you know what all of the col names are.
+  # and the order.
+  def insert_one(table_name, vals)
+    begin
+      # "INSERT INTO Writers(Name) VALUES('Jack London')"
+      con = Mysql.new(@url, @user, @password, @db_name)
+      
+      rs = con.query("INSERT INTO " + table_name + " (" + col_name + ") VALUES (" + col_names + ")")
+      result = rs.fetch_row
+      #puts result.inspect
+      self.hash_arrays(self.read_col_names(table_name), result)
+    rescue Mysql::Error => e
+      self.print_error e
+    ensure
+      con.close if con
+    end
+  end
+
+  # READ METHODS
+  ############################################################################
+  
+  # reads the last row of a given table.
+  # sort by column name.
+  # will use this to resume the scraper to determine where you left off.
+  # it returns an array of values.
+  def read_last_row(table_name, col_name)
+    begin
+      con = Mysql.new(@url, @user, @password, @db_name)
+      rs = con.query("SELECT * FROM " + table_name + " ORDER BY " + col_name + " DESC")
+      result = rs.fetch_row
+      #puts result.inspect
+      self.hash_arrays(self.read_col_names(table_name), result)
+    rescue Mysql::Error => e
+      self.print_error e
+    ensure
+      con.close if con
+    end
+  end
+
+  # read a single where condition and returns a row.
+  # useful for the zip retrival.
+  # probably can add a hash later
+  def read_with_one_param(table_name, col_name, col_val)
+    begin
+      con = Mysql.new(@url, @user, @password, @db_name)
+      rs = con.query("SELECT * FROM " + table_name + " WHERE " + col_name + "='" + col_val + "'")
+      result = rs.fetch_row
+      #puts result.inspect
+      self.hash_arrays(self.read_col_names(table_name), result)
+    rescue Mysql::Error => e
+      self.print_error e
+    ensure
+      con.close if con
+    end
+  end
+  
+  # a simple helper to read column names.
+  # used later to tag array results with column names.
+  # so the results will be easier to work with.
+  def read_col_names(table_name)
+    begin
+      con = Mysql.new(@url, @user, @password, @db_name)
+      
+      # stores results.
+      col_names = []
+      
+      rs = con.query("SELECT * FROM " + table_name)
+      rs.fetch_fields.each_with_index do |info, i|
+        col_names.push info.name
+      end
+      
+      col_names
+    rescue Mysql::Error => e
+      self.print_error e
+    ensure
+      con.close if con
+    end
+  end
+
+  # HELPER METHODS
+  ############################################################################
+  
+  # debug method.
+  def num_rows(table_name)
+    begin
+      con = Mysql.new(@url, @user, @password, @db_name)
+      rs = con.query("SELECT * FROM " + table_name)
+      n_rows = rs.num_rows
+    rescue Mysql::Error => e
+      self.print_error e
+    ensure
+      con.close if con
+    end
+  end
+  
+  # MISC METHODS
+  ############################################################################
+  # not used directly but used by other methods in this class.
+
+  # creates a hash map off 2 arrays.
+  # basically the read functions from sql return an array.
+  # i made a col name function and using those 2 things, i'll create a hash
+  # so things are easier to get at.
+  #
+  # making an assumption that both are the same size and not nil.
+  def hash_arrays(col_names, col_result)
+    result = Hash.new
+    
+    # using each syntax. assuming order of the results are aligned with the name.
+    col_names.each_with_index do |item, i|
+      result[item] = col_result[i]
+    end
+    
+    result
+  end
+
+  # returns all hash keys as a space seperated strinng.
+  # needed for mysel inserts.
+  def hash_get_keys_as_str(some_hash)
+    final_str = ""
+    first_val = true
+    
+    some_hash.keys.each do |item|
+      # first value does not have an intial space.
+      if first_val
+        first_val = false
+        final_str += item
+      else
+        # all other items. add a space first.
+        final_str += ", " + item
+      end
+    end
+    
+    final_str
+  end
+
+  # returns all hash keys as a space seperated strinng.
+  # needed for mysel inserts.
+  # this adds quotes...
+  # if you need null values...
+  # 
+  # probably need to fix this when you see numbers in the hash.
+  def hash_get_values_as_str(some_hash)
+    final_str = ""
+    first_val = true
+    
+    some_hash.keys.each do |item|
+      # first value does not have an intial space.
+      if first_val
+        first_val = false
+        final_str += item
+      end
+      
+      # all other items. add a space first.
+      final_str += " " + item
+    end
+    
+    final_str
+  end
+
+  # pass it sql error object and it will print some useful info.
+  def print_error(e)
+    puts "Error code: #{e.errno}"
+    puts "Error message: #{e.error}"
+    puts "Error SQLSTATE: #{e.sqlstate}" if e.respond_to?("sqlstate")
+  end
+
+  # probably dont' need this.
+  # this was the inntial test ot see if thing were working.
+  # grabbed from some website.
   def server_version
     begin
-      db = Mysql.real_connect("localhost", "root", "password", "test01")
-      puts "Server version: " + db.get_server_info
+      con = Mysql.real_connect(@url, @user, @password, @db_name)
+      "Server version: " + con.get_server_info
     rescue Mysql::Error => e
-      puts "Error code: #{e.errno}"
-      puts "Error message: #{e.error}"
-      puts "Error SQLSTATE: #{e.sqlstate}" if e.respond_to?("sqlstate")
+      self.print_error e  
     ensure
-      db.close if db
+      con.close if con
     end
   end
 
