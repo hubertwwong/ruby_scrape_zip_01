38adda260fdea2d17c636a816219452616041380
Hubert Wong <hubertwwong@gmail.com>
2013-08-09 18:02:33 -0700

    a bit closer. need to get the selector to work.

--- spec/features/test01_spec.rb
+++ spec/features/test01_spec.rb
@@ -1,30 +1,21 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "usps zip", :type => :feature do
-  it "be present" do
+  xit "be present" do
     visit "https://tools.usps.com/go/ZipLookupAction!input.action"
     
     expect(page).to have_content 'Street Address'
     #save_and_open_page
   end
   
-  it "can click on the zip code" do
+  xit "can click on the zip code" do
     visit "https://tools.usps.com/go/ZipLookupAction!input.action"
     click_link("Cities by ZIP Code", exact: false)
     
     expect(page).to have_content 'Get a list of all the cities in a ZIP Code'
   end
   
-  it "success zip." do
-    visit "https://tools.usps.com/go/ZipLookupAction!input.action"
-    click_link("Cities by ZIP Code", exact: false)
-    fill_in 'tZip', :with => '91770'
-    click_button 'Find'
-    
-    expect(page).to have_content 'Rosemead'
-  end
-  
-  it "unsuccessful zip." do
+  xit "unsuccessful zip." do
     visit "https://tools.usps.com/go/ZipLookupAction!input.action"
     click_link("Cities by ZIP Code", exact: false)
     fill_in 'tZip', :with => '00000'
@@ -32,4 +23,29 @@ describe "usps zip", :type => :feature do
     
     expect(page).to have_content 'Sorry'
   end
+  
+  xit "success zip." do
+    visit "https://tools.usps.com/go/ZipLookupAction!input.action"
+    click_link("Cities by ZIP Code", exact: false)
+    fill_in 'tZip', :with => '90026'
+    click_button 'Find'
+    
+    expect(page).to have_content 'Los Angeles'
+  end
+  
+  it "successful zip also returning city and state." do
+    visit "https://tools.usps.com/go/ZipLookupAction!input.action"
+    click_link("Cities by ZIP Code", exact: false)
+    fill_in 'tZip', :with => '90026'
+    click_button 'Find'
+    
+    within("div#result-cities") do
+      expect(page).to have_content 'Los Angeles'
+    end
+    
+    #find(:xpath, '//li[contains(.//a[@href = "#"]/text(), "foo")]').value
+    #puts find(:xpath, '//div#result-cities/h3:first-child').value
+    #puts "----"
+  end
+  
 end 
